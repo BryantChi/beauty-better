4f8970a55cf06fb85ce4c81e5877de6342234217
BryantChi <bryantchi.work@gmail.com>
2024-04-12 09:13:27 +0800

    [MOD] add about v-3 & modify header

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,16 +5,17 @@
 
     <!-- about_area_start -->
     <div class="about_area">
-        <div class="text-center mb-5">
-            <a href="javascript:void(0)" class="boxed-btn mx-1" onclick="view(1)">版本一</a>
-            <a href="javascript:void(0)" class="boxed-btn mx-1" onclick="view(2)">版本二</a>
+        <div class="text-center mb-5 d-flex justify-content-center">
+            <a href="javascript:void(0)" class="boxed-btn m-1" onclick="view(1)">V1</a>
+            <a href="javascript:void(0)" class="boxed-btn m-1" onclick="view(2)">V2</a>
+            <a href="javascript:void(0)" class="boxed-btn m-1" onclick="view(3)">V3</a>
         </div>
 
-        <div class="container">
+        <div class="container d-none2">
 
             <div class="row justify-content-center align-items-center view1" style="margin-bottom: 12rem;">
-                <div class="col-xl-3 col-md-4">
-                    <div class="about_thumb p-5" style="background-color: #452915;box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 15px;">
+                <div class="col-xl-3 col-md-4 ">
+                    <div class="about_thumb p-5 w-d-75 mx-auto" style="background-color: #452915;box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 15px;">
                         <img src="images/about/資產 1.webp" class="img-fluid" alt="">
                     </div>
                 </div>
@@ -42,8 +43,8 @@
             </div>
 
             <div class="row justify-content-center align-items-center mb-5 pb-5 view2" style="margin-bottom: 15rem;">
-                <div class="col-xl-3 col-md-4 mb-md-auto mb-4">
-                    <div class="about_thumb p-5" style="background-color: #452915;box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 2rem;">
+                <div class="col-xl-3 col-md-4 mb-md-0 mb-4 align-self-center2">
+                    <div class="about_thumb p-5 w-d-75 mx-auto" style="background-color: #452915;box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 1rem;">
                         <img src="images/about/資產 1.webp" class="img-fluid" alt="">
                     </div>
                 </div>
@@ -64,7 +65,9 @@
                         </ul>
                         <a href="#" class="boxed-btn">Services</a> --}}
                         <div class="about_content table-responsive">
-                            本院醫療團隊的宗旨在打造客戶專有的美上美，無論是身體曲線，或精緻立體的五官加上臉部輪廓緊緻年輕，客戶想改變的需求，我們都以認真熱誠的態度去面對，用專業精準的技術創造適合每一位顧客的專屬美麗。
+                            <h2 class="mb-3">美美上美診所｜讓你成為更美的自己</h2>
+                            <p>我們致力於為您提供專業的醫學美容服務，我們秉持著一個使命：為每位客戶量身打造專屬美上美。</p>
+                            <p>無論是身體曲線，或精緻立體的五官加上臉部輪廓緊緻年輕，客戶想改變的需求，我們都以認真熱誠的態度去面對，用專業精準的技術創造適合每一位顧客的專屬美麗。</p>
                         </div>
                     </div>
                 </div>
@@ -73,21 +76,14 @@
             <div class="row justify-content-around align-items-center mb-5 py-5 view2" style="margin-bottom: 15rem;">
                 <div class="col-xl-6 col-md-7 order-1 order-md-2 mb-md-auto mb-4">
                     <div class="about_thumb">
-                        <img src="images/about-01.jpg" class="img-fluid" style="box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 10rem;" alt="">
+                        <img src="images//about/about-03.jpg" class="img-fluid" style="box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 10rem;" alt="">
                     </div>
                 </div>
                 <div class="col-xl-5 col-md-5 order-2 order-md-1">
                     <div class="about_info">
-                        {{-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo
-                            viverra maecenas accumsan lacus vel facilisis. </p>
-                        <ul class="about_list">
-                            <li>Consectetur adipiscing sed do eiusmod. </li>
-                            <li>Eiusmod tempor incididunt labore. </li>
-                        </ul>
-                        <a href="#" class="boxed-btn">Services</a> --}}
-                        <div class="about_content table-responsive mt-md-5 pt-md-5">
-                            在醫美診所林立的多元市場中，我們持著專業醫療人員盡心盡力的態度，提供顧客美容美體的整體規劃、統合建議，站在顧客的角度思考，絕不強迫推銷，並力求所有療程設計都可以切合顧客需要，保障顧客的安全和權益。
+                        <div class="about_content table-responsive">
+                            <h2 class="mb-3">讓我們協助你擺脫歲月的痕跡</h2>
+                            <p class="">在醫美診所林立的多元市場中，我們持著專業醫療人員盡心盡力的態度，提供顧客美容美體的整體規劃、統合建議，站在顧客的角度思考，絕不強迫推銷，並力求所有療程設計都可以切合顧客需要，保障顧客的安全和權益，真誠服務貫穿始終，從首次接觸到後期追蹤，我們都以顧客需求為出發點，用心聆聽、細心解答，每位顧客都能感受到像家人一般溫暖而親切的關注。</p>
                         </div>
                     </div>
                 </div>
@@ -96,11 +92,36 @@
             <div class="row justify-content-around align-items-center mb-5 py-5 view2" style="margin-bottom: 10rem;">
                 <div class="col-xl-6 col-md-7 mb-md-auto mb-4">
                     <div class="about_thumb">
-                        <img src="images/about/about-02.webp" class="img-fluid" style="box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 10rem;" alt="">
+                        <img src="images/about/about-04.jpg" class="img-fluid about_area_3_img" style="height: 23rem;box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 10rem;" alt="">
                     </div>
                 </div>
                 <div class="col-xl-5 col-md-5">
                     <div class="about_info">
+                        <div class="about_content table-responsive">
+                            <h2 class="mb-3">為何選擇我們？</h2>
+                            <p>選擇美美上美診所，您也同時選擇了專業、安全和真誠，我們提供的不僅是解決方案，更是一段旅程——從第一次咨詢到最終實現夢想之美。</p>
+                            <p>緊密團隊合作是我們的特點，從醫師、護士乃至行政單位，除稱職扮演自己角色外，整個團隊就像個大家庭，上下一心將服務做到出色的服務，安全至上是我們堅持不懈的原則，所有治療計劃都基於顧客具體需求定制，並在完善細節中確保每項手術與處理高效而安全，希望這種溫暖體貼的氛圍，能讓顧客有踏實穩當的安全感，讓顧客與我們緊密相連，藉由我們的牽引，讓每一位走進美上美的人邁向成為更美的自己及更幸福的境界。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid view3 p-0">
+            <div class="row justify-content-center align-items-center mb-5 pb-5 mx-0" style="margin-bottom: 15rem;">
+                {{-- <div class="col-xl-3 col-md-4 mb-md-auto mb-4">
+                    <div class="about_thumb p-5" style="background-color: #452915;box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.558);opacity: 9;border-radius: 2rem;">
+                        <img src="images/about/資產 1.webp" class="img-fluid" alt="">
+                    </div>
+                </div> --}}
+                <div class="col-xl-6 offset-xl-2 col-md-6 px-md-0 px-3">
+                    <div class="about_info">
+                        <div class="section_title">
+                            <span class="sub_heading">About Us</span>
+                            <h3>MakeYou <br>
+                                A Better You</h3>
+                            <div class="seperator"></div>
+                        </div>
                         {{-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                             labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo
                             viverra maecenas accumsan lacus vel facilisis. </p>
@@ -109,45 +130,132 @@
                             <li>Eiusmod tempor incididunt labore. </li>
                         </ul>
                         <a href="#" class="boxed-btn">Services</a> --}}
-                        <div class="about_content table-responsive mt-md-5 pt-md-5">
-                            緊密團隊合作是我們的特點，從醫師、護士乃至行政單位，除稱職扮演自己角色外，整個團隊就像個大家庭，上下一心將服務做到出色的服務，希望這種溫暖體貼的氛圍，能讓顧客有踏實穩當的安全感。讓顧客與我們緊密相連，藉由我們的牽引，讓每一位走進美上美的人邁向成為更美的自己及更幸福的境界。
+                        <div class="about_content table-responsive">
+                            <h2 class="mb-3">美美上美診所｜讓你成為更美的自己</h2>
+                            <p>我們致力於為您提供專業的醫學美容服務，我們秉持著一個使命：為每位客戶量身打造專屬美上美。</p>
+                            <p>無論是身體曲線，或精緻立體的五官加上臉部輪廓緊緻年輕，客戶想改變的需求，我們都以認真熱誠的態度去面對，用專業精準的技術創造適合每一位顧客的專屬美麗。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-start align-items-center about_area_2 mb-5 py-5 mx-0"
+                style="">
+                <div class="col-xl-5 offset-xl-0 col-md-7 px-md-0 px-2">
+                    <div class="about_info ml-md-3">
+                        <div class="about_content table-responsive">
+                            <h2 class="mb-3 text-white">讓我們協助你擺脫歲月的痕跡</h2>
+                            <p class="text-white">在醫美診所林立的多元市場中，我們持著專業醫療人員盡心盡力的態度，提供顧客美容美體的整體規劃、統合建議，站在顧客的角度思考，絕不強迫推銷，並力求所有療程設計都可以切合顧客需要，保障顧客的安全和權益，真誠服務貫穿始終，從首次接觸到後期追蹤，我們都以顧客需求為出發點，用心聆聽、細心解答，每位顧客都能感受到像家人一般溫暖而親切的關注。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center align-items-center my-5 py-5 pl-md-0 mx-0" style="margin-bottom: 15rem;">
+                <div class="col-xl-5 col-md-5 mb-md-auto mb-4 px-md-0 px-3">
+                    <div class="about_thumb">
+                        <img src="images/about/about-04.jpg" class="img-fluid about_area_3_img" alt="">
+                    </div>
+                </div>
+                <div class="col-xl-7 offset-xl-0 col-md-7 px-md-5 px-3">
+                    <div class="about_info">
+                        <div class="about_content table-responsive">
+                            <h2 class="mb-3">為何選擇我們？</h2>
+                            <p>選擇美美上美診所，您也同時選擇了專業、安全和真誠，我們提供的不僅是解決方案，更是一段旅程——從第一次咨詢到最終實現夢想之美。</p>
+                            <p>緊密團隊合作是我們的特點，從醫師、護士乃至行政單位，除稱職扮演自己角色外，整個團隊就像個大家庭，上下一心將服務做到出色的服務，安全至上是我們堅持不懈的原則，所有治療計劃都基於顧客具體需求定制，並在完善細節中確保每項手術與處理高效而安全，希望這種溫暖體貼的氛圍，能讓顧客有踏實穩當的安全感，讓顧客與我們緊密相連，藉由我們的牽引，讓每一位走進美上美的人邁向成為更美的自己及更幸福的境界。</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
     <!-- about_area_end -->
+
+    <div class="about_sub_area">
+        <div class="container">
+            <div class="section_title mx-auto text-center">
+                <span class="sub_heading">Core Values</span>
+                <h3>美美上美的中心理念</h3>
+                <div class="seperator"></div>
+            </div>
+
+            <div class="row">
+                <div class="col-md mb-md-auto my-3">
+                    <div class="mb-4 text-center">
+                        <img src="images/about/about_sub_01.jpg" class="img-fluid about_sub_img" alt="">
+                    </div>
+                    <div class="single_about_sub mx-auto">
+                        <h3>安全醫療</h3>
+                        <p>安全是首要理念，針對個人提供術前與術後完善的照顧。</p>
+                    </div>
+                </div>
+                <div class="col-md mb-md-auto my-3">
+                    <div class="mb-4 text-center">
+                        <img src="images/about/about_sub_02.jpg" class="img-fluid about_sub_img" alt="">
+                    </div>
+                    <div class="single_about_sub mx-auto">
+                        <h3>諮詢規劃</h3>
+                        <p>提供完整、適切、專屬於您的美學建議，尊重顧客的意願，不強迫推銷。</p>
+                    </div>
+                </div>
+                <div class="col-md mb-md-auto my-3">
+                    <div class="mb-4 text-center">
+                        <img src="images/about/about_sub_03.jpg" class="img-fluid about_sub_img" alt="">
+                    </div>
+                    <div class="single_about_sub mx-auto">
+                        <h3>精進技術</h3>
+                        <p>醫師皆定期進修，帶回最新觀念及技術。並發表演講於各個學術研討會。</p>
+                    </div>
+                </div>
+                <div class="col-md mb-md-auto my-3">
+                    <div class="mb-4 text-center">
+                        <img src="images/about/about_sub_04.jpg" class="img-fluid about_sub_img" alt="">
+                    </div>
+                    <div class="single_about_sub mx-auto">
+                        <h3>顧客與員工</h3>
+                        <p>我們重視所有的顧客，也重視所有的員工。我們深信，有快樂的員工，才能提供顧客滿意的服務。</p>
+                    </div>
+                </div>
+                <div class="col-md mb-md-auto my-3">
+                    <div class="mb-4 text-center">
+                        <img src="images/about/about_sub_05.jpg" class="img-fluid about_sub_img" alt="">
+                    </div>
+                    <div class="single_about_sub mx-auto">
+                        <h3>細緻服務</h3>
+                        <p>從診所環境規劃、諮詢、醫療操作到術後照顧，完整提供您無微不至的服務。</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @push('custom_css')
+    <link rel="stylesheet" href="assets/css/about.css?v={{ time() }}">
     <style>
-        .about_area {
-            background-image: url("images/about/about-bg2.jpg");
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center center;
-        }
-
-        @media (max-width: 767px) {
-            .about_area {
-                background-image: url("images/about/about-bg-mob2.jpg");
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center center;
+        @media (max-width: 768px) {
+            .w-d-75 {
+                width: 75% !important;
             }
         }
     </style>
 @endpush
 @push('custom_scripts')
     <script>
-        view(1);
+        view(3);
         function view(type) {
             if (type == 1) {
                 $('.view1').show();
                 $('.view2').hide();
+                $('.view3').hide();
             } else if (type == 2) {
                 $('.view1').hide();
                 $('.view2').show();
+                $('.view3').hide();
+            } else if (type == 3) {
+                $('.view1').hide();
+                $('.view2').hide();
+                $('.view3').show();
             }
         }
     </script>
--- resources/views/layouts_main/header.blade.php
+++ resources/views/layouts_main/header.blade.php
@@ -4,29 +4,34 @@
         <div id="sticky-header" class="main-header-area white-bg" style="background: #5703863 !important;">
             <div class="container-fluid p-0">
                 <div class="row align-items-center justify-content-between no-gutters">
-                    <div class="col-xl-2 col-lg-2">
+                    <div class="col-xl-2 col-lg-2 d-lg-none d-block">
                         <div class="logo-img">
                             <a href="{{ route('index') }}">
                                 <img src="images/logo_full_bk.png" class="img-fluid img-logo" alt="">
                             </a>
                         </div>
                     </div>
-                    <div class="col-auto">
-                        <div class="main-menu  d-none d-lg-block">
+                    <div class="col-md">
+                        <div class="main-menu d-none d-lg-block">
                             {{-- request()->is('news') ? 'active' : '' --}}
                             <nav>
                                 <ul id="navigation">
-                                    <li><a class="{{ Request::is('index') ? 'active' : '' }}" href="{{ route('index') }}">首頁</a></li>
-                                    <li><a href="service.html">醫師團隊</a></li>
-                                    <li><a class="{{ Request::is('about') ? 'active' : '' }}" href="{{ route('about') }}">診所資訊</a></li>
-                                    <li><a href="#">療程項目 <i class="ti-angle-down"></i></a>
+                                    {{-- <li><a class="{{ Request::is('index') ? 'active' : '' }}" href="{{ route('index') }}">首頁</a></li> --}}
+                                    <li class="px-md-4"><a href="service.html">醫師團隊</a></li>
+                                    <li class="px-md-4"><a class="{{ Request::is('about') ? 'active' : '' }}" href="{{ route('about') }}">診所資訊</a></li>
+                                    <li class="px-md-4"><a href="#">療程項目 <i class="ti-angle-down"></i></a>
                                         <ul class="submenu">
                                             <li><a href="blog.html">blog</a></li>
                                             <li><a href="single-blog.html">single-blog</a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="service.html">美麗見證</a></li>
-                                    <li><a href="service.html">醫師專欄</a></li>
+                                    <li class="logo-img d-lg-inline-block d-none px-4">
+                                        <a href="{{ route('index') }}">
+                                            <img src="images/logo_full_bk.png" class="img-fluid img-logo" alt="">
+                                        </a>
+                                    </li>
+                                    <li class="px-md-4"><a href="service.html">美麗見證</a></li>
+                                    <li class="px-md-4"><a href="service.html">醫師專欄</a></li>
 
                                     {{-- <li><a href="project.html">Projects</a></li> --}}
                                     {{-- <li><a href="service.html">療程項目</a></li> --}}
@@ -36,7 +41,7 @@
                                             <li><a href="elements.html">elements</a></li>
                                         </ul>
                                     </li> --}}
-                                    {{-- <li><a href="contact.html">聯絡資訊</a></li> --}}
+                                    <li><a href="contact.html">聯絡資訊</a></li>
                                 </ul>
                             </nav>
                         </div>
