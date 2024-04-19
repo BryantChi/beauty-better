f6fb7034d988020375fe5de9724ecb3ce818db4f
BryantChi <bryantchi.work@gmail.com>
2024-04-20 00:34:39 +0800

    [MOD] modify link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -131,7 +131,7 @@
                         </div>
                     </div>
                     <div class="w-100 d-flex mt-3">
-                        <a href="javascript:void(0)" class="boxed-btn ml-auto">更多案例 >></a>
+                        <a href="{{ route('case') }}" class="boxed-btn ml-auto">更多案例 >></a>
                     </div>
                 </div>
             </div>
