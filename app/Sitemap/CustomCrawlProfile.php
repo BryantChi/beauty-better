b5a5db778535ab66b118cd630f34b125214a7c33
BryantChi <bryantchi.work@gmail.com>
2024-05-05 13:11:39 +0800

    [ADD] add sitemap

---
+++ app/Sitemap/CustomCrawlProfile.php
@@ -0,0 +1,16 @@
+<?php
+
+use Spatie\Crawler\CrawlProfiles\CrawlProfile;
+use Psr\Http\Message\UriInterface;
+
+class CustomCrawlProfile extends CrawlProfile
+{
+    public function shouldCrawl(UriInterface $url): bool
+    {
+        if ($url->getHost() !== 'localhost') {
+            return false;
+        }
+
+        return $url->getPath() === '/';
+    }
+}
