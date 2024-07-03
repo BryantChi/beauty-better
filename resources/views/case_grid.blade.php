02b8387247711d26ed61f99c6d4504bddd098c2b
BryantChi <bryantchi.work@gmail.com>
2024-07-03 14:30:23 +0800

    [MOD] modify case ui v2

--- resources/views/case_grid.blade.php
+++ resources/views/case_grid.blade.php
@@ -37,9 +37,9 @@
                                         <li><a href="javascript:void(0)"><i class="fas fa-flag"></i>
                                                 {{ DB::table('post_type_infos')->whereNull('deleted_at')->where('id', $post->post_type)->value('type_name') }}</a>
                                         </li>
-                                        <li><a href="#"><i class="fas fa-calendar-alt"></i>
+                                        {{-- <li><a href="#"><i class="fas fa-calendar-alt"></i>
                                             {{ \Carbon\Carbon::parse($post->created_at)->format('Y-m-d H:m') }}</a>
-                                        </li>
+                                        </li> --}}
                                     </ul>
 
                                     <div class="w-100 text-right">
