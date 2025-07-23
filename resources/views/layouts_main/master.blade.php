ce252332966b46cbd0db69860b17ef47d9a16a85
BryantChi <bryantchi.work@gmail.com>
2025-07-23 14:41:21 +0800

    [MOD] modify ui frontend

--- resources/views/layouts_main/master.blade.php
+++ resources/views/layouts_main/master.blade.php
@@ -55,12 +55,6 @@
     <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
-    <div class="loading">
-        <div class="loader">
-
-        </div>
-        <img src="{{ asset('images/logo_page.png') }}" alt="{{ config('app.name') }}">
-    </div>
 
     @include('layouts_main.header')
 
@@ -97,6 +91,14 @@
         </a>
 
     </div> --}}
+
+
+    <div class="loading">
+        <div class="loader">
+
+        </div>
+        <img src="{{ asset('images/logo_page.png') }}" alt="{{ config('app.name') }}">
+    </div>
 
     <!-- JS here -->
     <script src="{{ asset('assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
@@ -136,12 +138,12 @@
     @stack('custom_scripts')
 
     <script>
-        $(window).on('DOMContentLoaded', function() {
+        $(window).on('load', function() {
             // 在這裡放置當整個網頁載入完成時執行的程式碼
             console.log("Page and all resources are loaded!");
             setTimeout(function () {
                 $('.loading').fadeOut();
-            }, 250);
+            }, 500);
 
             // 可以進行需要在所有資源載入完成後執行的工作
         });
